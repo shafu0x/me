a3274eaf4d0b1a3ff6d4e97aa535b6286c445a0a
shafu <shafu0x@gmail.com>
2026-03-29 15:05:14 -0400

    update education to note dropped out of Tübingen

--- app/page.tsx
+++ app/page.tsx
@@ -162,7 +162,7 @@ export default function Home() {
         <b>education</b>
         <br />
         <br />
-        University of T&uuml;bingen - Master, Machine Learning (2018-2019)
+        University of T&uuml;bingen - Master, Machine Learning (2018-2019, dropped out)
         <br />
         Westf&auml;lische Hochschule - B.Sc., Computer Science (2014-2017)
       </div>
